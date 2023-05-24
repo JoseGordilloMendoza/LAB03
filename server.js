28f35acdb5149915787f4c52e99c9fec532cfedf
Rommel Abraham Chambi Velasquez <rchambive@unsa.edu.pe>
2023-05-24 15:04:24 -0500

    Se agrego una escucha a la ruta files y que devuelve un array con los nombres de los archivos markdown

--- server.js
+++ server.js
@@ -7,9 +7,24 @@ const MarkdownIt = require('markdown-it'),
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
 
 // Ruta principal del servidor
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta para obtener la lista de archivos Markdown disponibles
+app.get('/files', (req, res) => {
+  fs.readdir(__dirname + '/markdown', (err, files) => {
+    if (err) {
+      res.status(500).json({ error: 'Error al leer los archivos Markdown' });
+    } else {
+      res.json({ files });
+    }
+  });
 });
